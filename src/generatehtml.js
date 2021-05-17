837731a26e6ade0c9e55fb0e744d75f0c344ee6c
Lisa Barton <lisabarton23@yahoo.com>
2021-05-17 14:58:53 -0600

    updated to use sub classes

--- src/generatehtml.js
+++ src/generatehtml.js
@@ -1,1 +1,33 @@
 //this is where the functions to create the HTML will live if they cant all be on the index.js
+//Employee
+
+//`getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+//Manager 
+// `Manager` will also have the following:
+
+// * `officeNumber`
+
+// * `getRole()`&mdash;overridden to return `'Manager'`
+
+
+//Engineer
+
+// * `github`&mdash;GitHub username
+
+// * `getGithub()`
+
+// * `getRole()`&mdash;overridden to return `'Engineer'`
+
+//intern
+// * `school`
+
+// * `getSchool()`
+
+// * `getRole()`&mdash;overridden to return `'Intern'`
